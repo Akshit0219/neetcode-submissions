514f6e68f33b21ce088e11b6aa6db5ed224f40e0
Akshit0219 <akshitjawla1999@gmail.com>
2026-08-13 19:46:36 +0530

    Add: intersection-of-two-arrays - submission-0

---
+++ Data Structures & Algorithms/intersection-of-two-arrays/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int,int>mpp1,mpp2;
+        for(auto it:nums1){
+         mpp1[it]++;
+        }
+        for(auto it:nums2){
+         mpp2[it]++;
+        }
+        vector<int>ans;
+        for(auto it: mpp1){
+           if(mpp2[it.first]>0){
+            ans.push_back(it.first);
+           }
+        }
+        return ans;
+    }
+};
