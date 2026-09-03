4764de1994d70c113117c2242a2d1c97f39cde68
Akshit0219 <akshitjawla1999@gmail.com>
2026-09-03 22:11:11 +0530

    Add: rearrange-array-elements-by-sign - submission-0

---
+++ Data Structures & Algorithms/rearrange-array-elements-by-sign/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>arr(n);
+        int i=0,j=1,k=0;
+        for(int k=0;k<nums.size();k++){
+            if(nums[k]>0){
+                arr[i]=nums[k];
+                i+=2;
+            }else{
+                arr[j]=nums[k];
+                j+=2;
+            }
+        }
+        return arr;
+
+    }
+};
+ 
